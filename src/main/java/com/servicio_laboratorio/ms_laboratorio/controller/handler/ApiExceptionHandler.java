4f7184e17872758f86a49f9f59638e216141ca8a
PabloDev <pe.philimon@duocuc.cl>
2026-05-16 19:39:06 -0400

    se agrego ExceptionHandler al microservicio

---
+++ src/main/java/com/servicio_laboratorio/ms_laboratorio/controller/handler/ApiExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.servicio_laboratorio.ms_laboratorio.controller.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(
+                        java.util.stream.Collectors.toMap(
+                                fieldError -> fieldError.getField(),
+                                fieldError -> fieldError.getDefaultMessage()
+                        )
+                );
+
+        logger.error("Errores de validación en ms-laboratorio: {}", errors);
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("message", ex.getMessage());
+
+        logger.error("Error en ms-laboratorio: {}", ex.getMessage());
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
